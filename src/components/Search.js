c7d51f8471591befed8236160fb8311f4289ec68
Shalev Raz Israel <shalev280804@gmail.com>
2022-09-01 00:20:22 +0300

    Improved search engine performence

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState, useTransition } from 'react';
 import { fetchTop250 } from '../axios';
 import LoadingScreen from './LoadingScreen';
 import MovieListViewer from './MovieListViewer';
@@ -6,6 +6,7 @@ import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import './Search.css';
 import { APIContext } from '../APIContext';
+import { LinearProgress } from '@mui/material';
 
 const movieToFilterString = (movie) => {
 	return movie.title + movie.fullTitle + movie.crew + movie.year;
@@ -17,6 +18,7 @@ const filterOptions = createFilterOptions({
 const Search = () => {
 	const [top250Movies, setTop250Movies] = useState([]);
 	const [topMoviesOptions, setTopMoviesOptions] = useState([]);
+	const [pending, startTransition] = useTransition();
 	const [query, setQuery] = useState('');
 	const [apiKey] = useContext(APIContext);
 
@@ -27,7 +29,9 @@ const Search = () => {
 			value = value.fullTitle;
 		}
 		setQuery(value);
-		setTopMoviesOptions(top250Movies.filter((movie) => movieToFilterString(movie).toLowerCase().includes(value.toLowerCase())));
+		startTransition(() => {
+			setTopMoviesOptions(top250Movies.filter((movie) => movieToFilterString(movie).toLowerCase().includes(value.toLowerCase())));
+		});
 	}
 
 	useEffect(() => {
@@ -48,6 +52,7 @@ const Search = () => {
 	return top250Movies ? (
 		<div className="search">
 			<h1>Search Movies</h1>
+			{pending && <LinearProgress />}
 			<Autocomplete freeSolo autoComplete inputValue={query} onChange={updateQuery} onInputChange={updateQuery} filterOptions={filterOptions} getOptionLabel={(movie) => movie.fullTitle || (topMoviesOptions.length && topMoviesOptions[0].fullTitle) || movie} options={topMoviesOptions} renderInput={(params) => <TextField {...params} label="Movie" />} />
 			<br />
 			<br />
