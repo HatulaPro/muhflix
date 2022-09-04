0380cf4e97a193c88a810d73f09b0f44a0de1f56
Shalev Raz Israel <shalev280804@gmail.com>
2022-09-04 16:02:03 +0300

    More performence improvements to MovieCast

--- src/components/MovieCast.js
+++ src/components/MovieCast.js
@@ -1,16 +1,35 @@
-import { useState } from 'react';
+import { useMemo } from 'react';
+import { useState, useDeferredValue } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ImageLoader from './ImageLoader';
 import './MovieCast.css';
 
+const MIN_ACTORS_ON_SCREEN = 4;
+
 const MovieCast = ({ actorList }) => {
 	const [actorsStart, setActorsStart] = useState(0);
-
-	console.log(actorList.length);
+	const actorsStartDeferred = useDeferredValue(actorsStart);
+	const actorsElementsList = useMemo(
+		() =>
+			actorList &&
+			actorList.map((actor, index) =>
+				index >= actorsStartDeferred && index <= actorsStartDeferred + MIN_ACTORS_ON_SCREEN ? (
+					<CSSTransition key={actor.id} timeout={{ appear: 0, enter: 100, exit: 50 }} classNames="movieCast_shrink">
+						<div className="movieCast_actor">
+							<ImageLoader src={actor.image} alt={actor.name} />
+							<p>{actor.name}</p>
+							<p>{actor.asCharacter}</p>
+						</div>
+					</CSSTransition>
+				) : null
+			),
+		[actorsStartDeferred, actorList]
+	);
+	// const actorsStartDeferred = actorsStart;
 
 	function moveActorsForward() {
-		const practicalLength = actorList.length - 2;
-		setActorsStart(Math.min(actorsStart + 1, practicalLength - 1) % practicalLength);
+		const practicalLength = actorList.length - MIN_ACTORS_ON_SCREEN;
+		setActorsStart(Math.min(actorsStart + 1, practicalLength));
 	}
 	function moveActorsBack() {
 		setActorsStart(Math.max(actorsStart - 1, 0));
@@ -21,18 +40,7 @@ const MovieCast = ({ actorList }) => {
 				&lt;
 			</button>
 			<TransitionGroup component={'div'} className="movieCast_castScroller" appear={false}>
-				{actorList &&
-					actorList.map((actor, index) =>
-						index >= actorsStart && index < actorsStart + 6 ? (
-							<CSSTransition key={actor.id} timeout={{ appear: 0, enter: 400, exit: 400 }} classNames="movieCast_shrink">
-								<div className="movieCast_actor">
-									<ImageLoader src={actor.image} alt={actor.name} />
-									<p>{actor.name}</p>
-									<p>{actor.asCharacter}</p>
-								</div>
-							</CSSTransition>
-						) : null
-					)}
+				{actorsElementsList}
 			</TransitionGroup>
 			<button className="btn movieCast_scrollButtons" onClick={moveActorsForward}>
 				&gt;
