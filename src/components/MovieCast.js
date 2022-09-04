51535894244ee7b5ff35cfa21bd9f9cc68ea2713
Shalev Raz Israel <shalev280804@gmail.com>
2022-09-04 16:14:05 +0300

    The number of actors showed now depends on the screen's width

--- src/components/MovieCast.js
+++ src/components/MovieCast.js
@@ -1,31 +1,28 @@
-import { useMemo } from 'react';
-import { useState, useDeferredValue } from 'react';
+import { useMemo, useRef, useState, useDeferredValue } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ImageLoader from './ImageLoader';
 import './MovieCast.css';
 
-const MIN_ACTORS_ON_SCREEN = 4;
-
 const MovieCast = ({ actorList }) => {
 	const [actorsStart, setActorsStart] = useState(0);
+	const parentRef = useRef(null);
 	const actorsStartDeferred = useDeferredValue(actorsStart);
-	const actorsElementsList = useMemo(
-		() =>
-			actorList &&
-			actorList.map((actor, index) =>
-				index >= actorsStartDeferred && index <= actorsStartDeferred + MIN_ACTORS_ON_SCREEN ? (
-					<CSSTransition key={actor.id} timeout={{ appear: 0, enter: 100, exit: 50 }} classNames="movieCast_shrink">
-						<div className="movieCast_actor">
-							<ImageLoader src={actor.image} alt={actor.name} />
-							<p>{actor.name}</p>
-							<p>{actor.asCharacter}</p>
-						</div>
-					</CSSTransition>
-				) : null
-			),
-		[actorsStartDeferred, actorList]
-	);
-	// const actorsStartDeferred = actorsStart;
+
+	// Calculating the space each actor has
+	const MIN_ACTORS_ON_SCREEN = Math.floor((parentRef.current?.clientWidth - 120) / 160 || 4);
+	const actorsElementsList = useMemo(() => {
+		return actorList.map((actor, index) =>
+			index >= actorsStartDeferred && index <= actorsStartDeferred + MIN_ACTORS_ON_SCREEN ? (
+				<CSSTransition key={actor.id} timeout={{ appear: 0, enter: 100, exit: 50 }} classNames="movieCast_shrink">
+					<div className="movieCast_actor">
+						<ImageLoader src={actor.image} alt={actor.name} />
+						<p>{actor.name}</p>
+						<p>{actor.asCharacter}</p>
+					</div>
+				</CSSTransition>
+			) : null
+		);
+	}, [actorsStartDeferred, actorList]);
 
 	function moveActorsForward() {
 		const practicalLength = actorList.length - MIN_ACTORS_ON_SCREEN;
@@ -35,7 +32,7 @@ const MovieCast = ({ actorList }) => {
 		setActorsStart(Math.max(actorsStart - 1, 0));
 	}
 	return (
-		<div className="movieCast_castContent">
+		<div className="movieCast_castContent" ref={parentRef}>
 			<button className="btn movieCast_scrollButtons" onClick={moveActorsBack}>
 				&lt;
 			</button>
