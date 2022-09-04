cd04fde4760e18fa4511749fba423b6e0f4cc139
Shalev Raz Israel <shalev280804@gmail.com>
2022-09-04 19:22:29 +0300

    Using the browser's API to improve scrolling speed

--- src/components/MovieCast.js
+++ src/components/MovieCast.js
@@ -1,28 +1,23 @@
-import { useMemo, useRef, useState, useDeferredValue } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import ImageLoader from './ImageLoader';
+import { useMemo, useRef, useState, useEffect } from 'react';
+import { TransitionGroup } from 'react-transition-group';
 import './MovieCast.css';
 
 const MovieCast = ({ actorList }) => {
 	const [actorsStart, setActorsStart] = useState(0);
 	const parentRef = useRef(null);
-	const actorsStartDeferred = useDeferredValue(actorsStart);
+	const currentActor = useRef(null);
 
 	// Calculating the space each actor has
 	const MIN_ACTORS_ON_SCREEN = Math.floor((parentRef.current?.clientWidth - 120) / 160 || 4);
 	const actorsElementsList = useMemo(() => {
-		return actorList.map((actor, index) =>
-			index >= actorsStartDeferred && index <= actorsStartDeferred + MIN_ACTORS_ON_SCREEN ? (
-				<CSSTransition key={actor.id} timeout={{ appear: 0, enter: 100, exit: 50 }} classNames="movieCast_shrink">
-					<div className="movieCast_actor">
-						<ImageLoader src={actor.image} alt={actor.name} />
-						<p>{actor.name}</p>
-						<p>{actor.asCharacter}</p>
-					</div>
-				</CSSTransition>
-			) : null
-		);
-	}, [actorsStartDeferred, actorList]);
+		return actorList.map((actor, index) => (
+			<div key={actor.id} className="movieCast_actor" ref={index === actorsStart ? currentActor : undefined}>
+				<img src={actor.image} alt={actor.name} />
+				<p>{actor.name}</p>
+				<p>{actor.asCharacter}</p>
+			</div>
+		));
+	}, [actorsStart, actorList]);
 
 	function moveActorsForward() {
 		const practicalLength = actorList.length - MIN_ACTORS_ON_SCREEN;
@@ -31,6 +26,12 @@ const MovieCast = ({ actorList }) => {
 	function moveActorsBack() {
 		setActorsStart(Math.max(actorsStart - 1, 0));
 	}
+
+	useEffect(() => {
+		if (!currentActor.current) return;
+		currentActor.current.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' });
+	}, [currentActor, actorsElementsList]);
+
 	return (
 		<div className="movieCast_castContent" ref={parentRef}>
 			<button className="btn movieCast_scrollButtons" onClick={moveActorsBack}>
